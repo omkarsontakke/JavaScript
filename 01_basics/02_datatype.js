e1d110b319a7661b4dc03b811465e5d7a543afe9
Omkar Sontakke <95247012+omkarsontakke@users.noreply.github.com>
2024-06-23 16:11:15 +0000

    Datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,15 @@
+"use strict"; // trate all js code as newer version
+
+// number => 2 to the power 53
+// bigint
+// string => "Omkar"
+// boolean => true or false
+// char => 'z'
+// null => standalon value //datatype //representation intentially
+// undefined  
+// symbol => uiqueness
+
+//Object
+
+console.log(typeof null);
+console.log(typeof undefined); 
