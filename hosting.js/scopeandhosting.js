0fe778416647f603c8aa69a660cc2ed20af29d98
Omkar Sontakke <95247012+omkarsontakke@users.noreply.github.com>
2024-06-24 08:01:45 +0000

    scope notes

--- hosting.js/scopeandhosting.js
+++ hosting.js/scopeandhosting.js
@@ -1,4 +1,12 @@
 //************** Scope and hosting ********************** */
+// var
+// Declares a variable, optionally initializing it to a value.
+
+// let
+// Declares a block-scoped, local variable, optionally initializing it to a value.
+
+// const
+// Declares a block-scoped, read-only named constant.
 
 function one() {
     let username = "Omkar";
